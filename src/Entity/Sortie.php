226928a65c59a75be88d4bb29f081eb6b9b06415
Brewen LE MEE <brewen.lemee2021@campus-eni.fr>
2023-01-03 10:14:07 +0100

    update sortie

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -21,7 +21,7 @@ class Sortie
     private ?\DateTimeInterface $date = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $duree = null;
+    private ?integer $duree = null;
 
     #[ORM\Column]
     private ?int $nbParticipant = null;
@@ -65,12 +65,12 @@ class Sortie
         return $this;
     }
 
-    public function getDuree(): ?string
+    public function getDuree(): ?integer
     {
         return $this->duree;
     }
 
-    public function setDuree(string $duree): self
+    public function setDuree(integer $duree): self
     {
         $this->duree = $duree;
 
