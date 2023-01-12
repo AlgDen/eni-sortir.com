2c8478523355a1e3394a53c794b3328747f11c1b
Brewen LE MEE <brewen.lemee2021@campus-eni.fr>
2023-01-12 11:36:44 +0100

    Annulation sortie

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -47,6 +47,12 @@ class Sortie
     #[ORM\ManyToMany(targetEntity: User::class)]
     private Collection $inscrits;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $motif = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    private ?Site $site = null;
+
     public function __construct()
     {
         $this->inscrits = new ArrayCollection();
@@ -184,6 +190,30 @@ class Sortie
     public function removeInscrit(User $inscrit): self
     {
         $this->inscrits->removeElement($inscrit);
+
+        return $this;
+    }
+
+    public function getMotif(): ?string
+    {
+        return $this->motif;
+    }
+
+    public function setMotif(?string $motif): self
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    public function setSite(?Site $site): self
+    {
+        $this->site = $site;
 
         return $this;
     }
